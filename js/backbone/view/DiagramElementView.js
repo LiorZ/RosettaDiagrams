ee2546469cd5f86b927926c345130875354f79fb
lior <liorz1984@gmail.com>
2013-07-07 17:26:39 +0300

    fixing background image and creating an icon file

--- js/backbone/view/DiagramElementView.js
+++ js/backbone/view/DiagramElementView.js
@@ -38,7 +38,6 @@ $(function() {
 			app.EventAgg.bind('editDiagramElement',this.toggleHighlight);
 			var context = this;
 			jointObj.registerCallback('elementMoved',function(new_loc){
-				console.log(new_loc);
 				context.model.set('x',new_loc.x);
 				context.model.set('y',new_loc.y);
 			});
