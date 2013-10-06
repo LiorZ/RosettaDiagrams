810c1ea08ec3932add32403a48f10f38eaca52bc
lior <liorz1984@gmail.com>
2013-10-06 10:47:10 +0300

    Modified dir structure

--- tests/unit_tests/XMLToDiagram.js
+++ tests/unit_tests/XMLToDiagram.js
@@ -22,6 +22,11 @@ describe('Testing XMLToDiagram',function() {
 				          '../../js/lib/jquery/jquery.min.js',
 				          '../../js/lib/underscore/underscore-min.js',
 				          "../../js/lib/backbone/backbone.js",
+				          "../../js/backbone/view/XMLToDiagram.js",
+						  "../../js/backbone/collections/DiagramConnectionCollection.js",
+						  "../../js/backbone/collections/DiagramElementCollection.js",
+						  "../../js/backbone/model/Diagram.js",
+						  "../../js/backbone/view/appView.js",
 						  "../../js/backbone/model/Attribute.js",
 						  "../../js/backbone/model/TaskOAttribute.js",
 						  "../../js/backbone/model/InformationMessage.js",
@@ -29,14 +34,12 @@ describe('Testing XMLToDiagram',function() {
 						  "../../js/backbone/model/IFMover.js",
 						  "../../js/backbone/model/DiagramConnection.js",
 						  "../../js/backbone/model/DiagramContainment.js",
-						  "../../js/backbone/collections/DiagramElementCollection.js",
-						  "../../js/backbone/collections/DiagramConnectionCollection.js",
-						  "../../js/backbone/model/Diagram.js",
 				          '../../js/lib/jquery/xml2json/xml2json.js',
-				          '../../js/backbone/controllers/XMLToDiagram.js'
+				          '../../js/backbone/controllers/XMLToDiagram.js',
 			],
 			done: function(err,window){
 				   app = window.app;
+				   window.console = console;
 				   should.exist(app.DiagramElement);
 				   var domparser = new DOMParser();
 				   window.$.parseXML =function(xml_doc) { return domparser.parseFromString(xml_doc,'text/xml'); };
@@ -102,5 +105,28 @@ describe('Testing XMLToDiagram',function() {
 		edges[0].sourceId.should.eql('element_0');
 		edges[1].sourceId.should.eql('element_1');
 		edges[2].sourceId.should.eql('element_3');
+	});
+	
+	it("Retrieving an XML that contains nested mover", function(done) {
+		jq.ajax({
+            url : "http://localhost:8000/tests/unit_tests/files/test2.xml",
+            dataType: "text",
+            success : function (data,textStatus) {
+            	textStatus.should.eql('success');
+            	xml_str.should.not.eql(undefined);
+            	xml_str = data;
+            	done();
+            },
+            error:function(err){
+            	done(new Error(err));
+            }
+        });
+	});
+	
+	it("Testing nested mover structure", function() { 
+		var json = app.xmlToDiagramJSON(xml_str);
+		var element_json_arr = app.extractElementsFromJSON(json);
+		app.createBackboneElements(element_json_arr);
+
 	});
 });
